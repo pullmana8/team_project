a315e6dc5d262bafddf9a303d9cbac3b88c262fd
pullmana8 <pullmana8@gmail.com>
2016-11-12 04:12:41 -0600

    changed out the files, and cleaned up the folders

--- README.md
+++
@@ -1,3 +0,0 @@
-# Trivia Game
-## Owl-trageous Trivia Game
-### Welcome to our Owl-trageous Trivia Game. It's a Hoot!!
---
+++ js/mediummode.js
@@ -0,0 +1,324 @@
+(function() {
+  var questions = [{
+    category: "General Knowledge",
+    type: "multiple",
+    difficulty: "medium",
+    question: "According to the BBPA, what is the most common pub name in the UK?",
+    correctAnswer: "Red Lion",
+    choices: ["Royal Oak", "White Hart", "King&#039;s Head", "Red Lion"]
+  }, {
+    category: "General Knowledge",
+    type: "multiple",
+    difficulty: "medium",
+    question: "What is the world&#039;s most expensive spice by weight?",
+    correctAnswer: "Saffron",
+    choices: ["Cinnamon", "Cardamom", "Vanilla", "Saffron"]
+  }, {
+    category: "General Knowledge",
+    type: "multiple",
+    difficulty: "medium",
+    question: "This field is sometimes known as &ldquo;The Dismal Science.&rdquo;",
+    correctAnswer: "Economics",
+    choices: ["Philosophy", "Politics", "Physics", "Economics"]
+  }, {
+    category: "General Knowledge",
+    type: "multiple",
+    difficulty: "medium",
+    question: "What is the defining characteristic of someone who is described as hirsute?",
+    correctAnswer: "Hairy",
+    choices: ["Rude", "Funny", "Tall", "Hairy"]
+  }, {
+    category: "General Knowledge",
+    type: "multiple",
+    difficulty: "medium",
+    question: "In the Morse code, which letter is indicated by 3 dots? ",
+    correctAnswer: "S",
+    choices: ["O", "A", "C", "S"]
+  }, {
+    category: "General Knowledge",
+    type: "multiple",
+    difficulty: "medium",
+    question: "What does the &quot;G&quot; mean in &quot;G-Man&quot;?",
+    correctAnswer: "Government",
+    choices: ["Going", "Ghost", "Geronimo", "Government"]
+  }, {
+    category: "General Knowledge",
+    type: "multiple",
+    difficulty: "medium",
+    question: "Which language is NOT Indo-European?",
+    correctAnswer: "Hungarian",
+    choices: ["Russian", "Greek", "Latvian", "Hungarian"]
+  }, {
+    category: "General Knowledge",
+    type: "multiple",
+    difficulty: "medium",
+    question: "Which essential condiment is also known as Japanese horseradish?",
+    correctAnswer: "Wasabi",
+    choices: ["Mentsuyu", "Karashi", "Ponzu", "Wasabi"]
+  }, {
+    category: "General Knowledge",
+    type: "multiple",
+    difficulty: "medium",
+    question: "What is a &quot;dakimakura&quot;?",
+    correctAnswer: "A body pillow",
+    choices: ["A body pillow", "A Chinese meal, essentially composed of fish", "A yoga posture", "A word used to describe two people who truly love each other"]
+  }, {
+    category: "General Knowledge",
+    type: "multiple",
+    difficulty: "medium",
+    question: "What is the name of the very first video uploaded to YouTube?",
+    correctAnswer: "Me at the zoo",
+    choices: ["tribute", "carrie rides a truck", "Her new puppy from great grandpa vern.", "Me at the zoo"]
+  }, {
+    category: "General Knowledge",
+    type: "multiple",
+    difficulty: "medium",
+    question: "In 2013 how much money was lost by Nigerian scams?",
+    correctAnswer: "$12.7 Billion",
+    choices: ["$95 Million", "$956 Million", "$12.7 Billion", "$2.7 Billion"]
+  }, {
+    category: "General Knowledge",
+    type: "multiple",
+    difficulty: "medium",
+    question: "Which of these companies does NOT manufacture automobiles?",
+    correctAnswer: "Ducati",
+    choices: ["Nissan", "GMC", "Fiat", "Ducati"]
+  }, {
+    category: "General Knowledge",
+    type: "multiple",
+    difficulty: "medium",
+    question: "What was the destination of the missing flight MH370?",
+    correctAnswer: "Beijing",
+    choices: ["Kuala Lumpur", "Singapore", "Tokyo", "Beijing"]
+  }, {
+    category: "General Knowledge",
+    type: "multiple",
+    difficulty: "medium",
+    question: "A factiod is what?",
+    correctAnswer: "A falsehood",
+    choices: ["Useless trivia", "A falsehood", "A tiny fact", "A scientific figure"]
+  }, {
+    category: "General Knowledge",
+    type: "multiple",
+    difficulty: "medium",
+    question: "Which of the following buildings is example of a structure primarily built in the Art Deco architectural style?",
+    correctAnswer: "Niagara Mohawk Building",
+    choices: ["Taipei 101", "One Detroit Center", "Niagara Mohawk Building", "Westendstrasse 1"]
+  }, {
+    category: "General Knowledge",
+    type: "multiple",
+    difficulty: "medium",
+    question: "What did the Spanish autonomous community of Catalonia ban in 2010, that took effect in 2012?",
+    correctAnswer : "Bullfighting",
+    choices : ["Fiestas", "Bullfighting", "Flamenco", "Mariachi"]
+  }, {
+    category: "General Knowledge",
+    type: "multiple",
+    difficulty: "medium",
+    question: "Which Italian automobile manufacturer gained majority control of U.S. automobile manufacturer Chrysler in 2011?",
+    correctAnswer: "Fiat",
+    choices: ["Maserati", "Fiat", "Alfa Romeo", "Ferrari"]
+  }, {
+    category: "General Knowledge",
+    type: "multiple",
+    difficulty: "medium",
+    question: "What is the name given to Indian food cooked over charcoal in a clay oven?",
+    correctAnswer: "Tandoori",
+    choices: ["Biryani", "Tandoori", "Pani puri", "Tiki masala"]
+  }, {
+    category: "General Knowledge",
+    type: "multiple",
+    difficulty: "medium",
+    question: "What is the romanized Japanese word for &quot;university&quot;?",
+    correctAnswer: "Daigaku",
+    choices: ["Toshokan", "Daigaku", "Jimusho", "Shokudou"]
+  }, {
+    category: "General Knowledge",
+    type: "multiple",
+    difficulty: "medium",
+    question: "What is the Swedish word for &quot;window&quot;?",
+    correctAnswer: "F&ouml;nster",
+    choices: ["H&aring;l", "F&ouml;nster", "Sk&auml;rm", "Ruta"]
+  }, {
+    category: "General Knowledge",
+    type: "multiple",
+    difficulty: "medium",
+    question: "What is the Italian word for &quot;tomato&quot;?",
+    correctAnswer: "Pomodoro",
+    choices: ["Aglio", "Pomodoro", "Cipolla", "Peperoncino"]
+  }, {
+    category: "General Knowledge",
+    type: "multiple",
+    difficulty: "medium",
+    question: "What is the romanized Russian word for &quot;winter&quot;?",
+    correctAnswer: "Zima",
+    choices: ["Leto", "Zima", "Vesna", "Osen&#039;"]
+  }, {
+    category: "General Knowledge",
+    type: "multiple",
+    difficulty: "medium",
+    question: "What is the name of the popular animatronic singing fish prop, singing such hits such as &quot;Don&#039;t Worry, Be Happy&quot;?",
+    correctAnswer: "Big Mouth Billy Bass",
+    choices: ["Big Billy Bass", "Singing Fish", "Big Mouth Billy Bass", "Sardeen"]
+  }, {
+    category: "General Knowledge",
+    type: "multiple",
+    difficulty: "medium",
+    question: "What country saw a world record 315 million voters turn out for elections on May 20, 1991?",
+    correctAnswer: "India",
+    choices: ["United States of America", "India", "Soviet Union", "Poland"]
+  }, {
+    category: "General Knowledge",
+    type: "multiple",
+    difficulty: "medium",
+    question: "Which country has the record for the worlds largest orgy?",
+    correctAnswer: "Japan",
+    choices: ["Russia", "Japan", "Germany", "USA"]
+  }];
+
+var questionCounter = 0; //Tracks question number
+  var selections = []; //Array containing user choices
+  var quiz = $('#medium'); 
+
+  // Display initial question
+  displayNext();
+
+  // Click handler for the 'next' button
+  $('#next').on('click', function(e) {
+    e.preventDefault();
+
+    // Suspend click listener during fade animation
+    if (quiz.is(':animated')) {
+      return false;
+    }
+    choose();
+
+    // If no user selection, progress is stopped
+    if (isNaN(selections[questionCounter])) {
+      alert('Please make a selection!');
+    } else {
+      questionCounter++;
+      displayNext();
+    }
+  });
+
+  // Click handler for the 'prev' button
+  $('#prev').on('click', function(e) {
+    e.preventDefault();
+
+    if (quiz.is(':animated')) {
+      return false;
+    }
+    choose();
+    questionCounter--;
+    displayNext();
+  });
+
+  // Click handler for the 'Start Over' button
+  $('#start').on('click', function(e) {
+    e.preventDefault();
+
+    if (quiz.is(':animated')) {
+      return false;
+    }
+    questionCounter = 0;
+    selections = [];
+    displayNext();
+    $('#start').hide();
+  });
+
+  // Animates buttons on hover
+  $('.button').on('mouseenter', function() {
+    $(this).addClass('active');
+  });
+  $('.button').on('mouseleave', function() {
+    $(this).removeClass('active');
+  });
+
+  // Creates and returns the div that contains the questions and 
+  // the answer selections
+  function createQuestionElement(index) {
+    var qElement = $('<div>', {
+      id: 'question'
+    });
+
+    var header = $('<h4>Question ' + (index + 1) + ':</h4>');
+    qElement.append(header);
+
+    var question = $('<p>').append(questions[index].question);
+    qElement.append(question);
+
+    var radioButtons = createRadios(index);
+    qElement.append(radioButtons);
+
+    return qElement;
+  }
+
+  // Creates a list of the answer choices as radio inputs
+  function createRadios(index) {
+    var radioList = $('<ul>');
+    var item;
+    var input = '';
+    for (var i = 0; i < questions[index].choices.length; i++) {
+      item = $('<li>');
+      input = '<input type="radio" name="answer" value=' + i + ' />';
+      input += questions[index].choices[i];
+      item.append(input);
+      radioList.append(item);
+    }
+    return radioList;
+  }
+
+  // Reads the user selection and pushes the value to an array
+  function choose() {
+    selections[questionCounter] = +$('input[name="answer"]:checked').val();
+  }
+
+  // Displays next requested element
+  function displayNext() {
+    quiz.fadeOut(function() {
+      $('#question').remove();
+
+      if (questionCounter < questions.length) {
+        var nextQuestion = createQuestionElement(questionCounter);
+        quiz.append(nextQuestion).fadeIn();
+        if (!(isNaN(selections[questionCounter]))) {
+          $('input[value=' + selections[questionCounter] + ']').prop('checked', true);
+        }
+
+        // Controls display of 'prev' button
+        if (questionCounter === 1) {
+          $('#prev').show();
+        } else if (questionCounter === 0) {
+
+          $('#prev').hide();
+          $('#next').show();
+        }
+      } else {
+        var scoreElem = displayScore();
+        quiz.append(scoreElem).fadeIn();
+        $('#next').hide();
+        $('#prev').hide();
+        $('#start').show();
+      }
+    });
+  }
+
+  // Computes score and returns a paragraph element to be displayed
+  function displayScore() {
+    var score = $('<p>', {
+      id: 'question'
+    });
+
+    var numCorrect = 0;
+    for (var i = 0; i < selections.length; i++) {
+      if (selections[i] === questions[i].correctAnswer) {
+        numCorrect++;
+      }
+    }
+
+    score.append('You got ' + numCorrect + ' questions out of ' +
+      questions.length + ' right!!!');
+    return score;
+  }
+})();
